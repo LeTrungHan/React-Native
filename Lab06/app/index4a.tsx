12da4e74427b234bfcbf34a33583e6a034951d6f
TrungHan <letrunghan1692003@gmail.com>
2024-10-07 10:59:18 +0700

    Fixed embedded repository issue configured line endings and push Lab06

---
+++ Lab06/app/index4a.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Screen1 from "@/components/layout/Screen1/screen1";
+import { View } from "react-native-reanimated/lib/typescript/Animated";
+
+export default function index4a() {
+
+    return (
+        
+        <View>
+            screen1
+        </View>
+    );
+}   
+
---
+++ Lab06/app/index4b.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Screen_02 from "@/components/layout/Screen2/screen2";
+
+export default function index4b() {
+    return <Screen_02 />;
+    }
